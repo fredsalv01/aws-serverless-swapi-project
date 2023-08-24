e5e728cbd4937bde9ae78985e81cf7ae9ab5ad24
Freddy Morales <freddy.morales@usil.pe>
2023-08-24 08:13:17 -0500

    change to spanish create Character response

--- src/application/controllers/character/api/createCharacterController.ts
+++ src/application/controllers/character/api/createCharacterController.ts
@@ -1,17 +1,18 @@
 import { Handler } from "aws-lambda";
 import { CharacterService } from "../../../../infraestructure/services/characterService";
 import { Character } from "../../../../core/domain/entities/classes/Character";
+import { CharactersSpanish } from "../../../types";
 
 const characterService = new CharacterService();
 
-export const handler: Handler = async (
-  event
-): Promise<Character> => {
+export const handler: Handler = async (event): Promise<CharactersSpanish> => {
   const { body } = event;
 
   const data: Character = new Character(JSON.parse(body));
 
-  const result: Character =
-    await characterService.createCharacterWithBiography(data);
-  return result;
+  const result: Character = await characterService.createCharacterWithBiography(
+    data
+  );
+  
+  return result.changeFieldsToSpanish();
 };
